cacd8785a60a76466b1a28a86d52b78987b978f9
jhiedy <jrabadines@up.edu.ph>
2022-11-09 15:51:44 +0800

    create project

---
+++ week7-discussion-code-main/lib/providers/todo_provider.dart
@@ -0,0 +1,75 @@
+/*
+  Created by: Claizel Coubeili Cepe
+  Date: 27 October 2022
+  Description: Sample todo app with networking
+*/
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:week7_networking_discussion/models/todo_model.dart';
+// import '../api/todo_api.dart';
+import 'package:week7_networking_discussion/api/firebase_todo_api.dart';
+
+class TodoListProvider with ChangeNotifier {
+  // late TodoAPI todoAPI;
+  // late Future<List<Todo>> _todoList;
+  // late FirebaseTodoAPI firebaseService;
+
+  // TodoListProvider() {
+  //   todoAPI = TodoAPI();
+  //   fetchTodos();
+  //   firebaseService = FirebaseTodoAPI();
+  // }
+
+  // // getter
+  // Future<List<Todo>> get todo => _todoList;
+
+  // void fetchTodos() {
+  //   _todoList = todoAPI.fetchTodos();
+  //   notifyListeners();
+  // }
+
+  late FirebaseTodoAPI firebaseService;
+  late Stream<QuerySnapshot> _todosStream;
+  Todo? _selectedTodo;
+
+  TodoListProvider() {
+    firebaseService = FirebaseTodoAPI();
+    fetchTodos();
+  }
+
+  Stream<QuerySnapshot> get todos => _todosStream;
+
+  Todo get selected => _selectedTodo!;
+
+  changeSelectedTodo(Todo item) {
+    _selectedTodo = item;
+  }
+
+  fetchTodos() {
+    _todosStream = firebaseService.getAllTodos();
+    notifyListeners();
+  }
+
+  void addTodo(Todo item) async {
+    String message = await firebaseService.addTodo(item.toJson(item));
+    print(message);
+    notifyListeners();
+  }
+
+  void editTodo(int index, String newTitle) {
+    // _todoList[index].title = newTitle;
+    notifyListeners();
+  }
+
+  void deleteTodo() async {
+    String message = await firebaseService.deleteTodo(_selectedTodo!.id);
+    print(message);
+    notifyListeners();
+  }
+
+  void toggleStatus(int index, bool status) {
+    // _todoList[index].completed = status;
+    notifyListeners();
+  }
+}
